a20cb89cf82747e333e800c89eab65ef025872e9
Steven <o.steven@wustl.edu>
2024-02-06 22:07:05 -0600

    done

---
+++ add.py
@@ -0,0 +1,5 @@
+Num1 = float(input("enter first number"))
+Num2 = float(input("enter second number"))
+result = Num1 + Num2
+print(result)
+
---
+++ hello.py
@@ -0,0 +1,6 @@
+num = float(input("enter a number"))
+count = 0
+while (count < num):
+   print("hello: " + str(count))
+   count = count + 1
+
